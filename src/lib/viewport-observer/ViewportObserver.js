793a48be491283dda15b945b54f96b2d04c88f36
Marielle Lange <mlange@widged.com>
2015-10-22 23:25:18 +1300

    [fix] reflecting changes in merged.js data

--- src/lib/viewport-observer/ViewportObserver.js
+++ src/lib/viewport-observer/ViewportObserver.js
@@ -56,3 +56,54 @@ export default class ViewportObserver {
 		return false;
 	}
 }
+
+
+/*
+
+// http://developer.telerik.com/featured/lazy-loading-images-on-the-web/
+
+function isElementInViewport (el) {
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */ /*
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */ /*
+    );
+}
+
+<img data-src="images/Robin.jpg" alt="" />
+
+// https://github.com/remotesynth/Lazy-Loading-Images
+// https://rawgit.com/remotesynth/Lazy-Loading-Images/master/custom.html
+// http://dinbror.dk/blazy/
+// http://www.smashingmagazine.com/2015/02/redefining-lazy-loading-with-lazy-load-xt/
+
+//these handlers will be removed once the images have loaded
+window.addEventListener("DOMContentLoaded", lazyLoadImages);
+window.addEventListener("load", lazyLoadImages);
+window.addEventListener("resize", lazyLoadImages);
+window.addEventListener("scroll", lazyLoadImages);
+
+function lazyLoadImages() {
+  var images = document.querySelectorAll("#main-wrapper img[data-src]"),
+      item;
+  // load images that have entered the viewport
+  [].forEach.call(images, function (item) {
+    if (isElementInViewport(item)) {
+      item.setAttribute("src",item.getAttribute("data-src"));
+      item.removeAttribute("data-src")
+    }
+  })
+  // if all the images are loaded, stop calling the handler
+  if (images.length == 0) {
+    window.removeEventListener("DOMContentLoaded", lazyLoadImages);
+    window.removeEventListener("load", lazyLoadImages);
+    window.removeEventListener("resize", lazyLoadImages);
+    window.removeEventListener("scroll", lazyLoadImages);
+  }
+}
+
+*/
--- src/main.es6.js
+++ src/main.es6.js
@@ -5,24 +5,25 @@ import ReactDOM          from 'react-dom';
 import BookmarksExplorer from './components/wg-bookmarks-explorer/BookmarksExplorer';
 import SearchInput       from './components/search-input/SearchInput.react.es6.js';
 
-
 let {Component,  PropTypes} = React;
 let {findDOMNode} = ReactDOM;
 
-
-
+// let x = require('https://spreadsheets.google.com/feeds/list/1M-dAy2_oaHugkx8kPYguA_IrYVEKWeejLPCFth506qY/od6/public/values?alt=json-in-script');
+// console.log(x);
+// view-source:https://spreadsheets.google.com/feeds/list/1M-dAy2_oaHugkx8kPYguA_IrYVEKWeejLPCFth506qY/od6/public/values?alt=json-in-script
 
 export default function main() {
 
 	let dataList = require('../dist/data/merged.js');
 	var list = dataList.split(/\n/).map((d) => {
 		d = d.replace(/\\/g,'\\\\');
-		d = d.replace(/<a href="(.*?)">/g, "<a href='().*?)'>");
-		let {url,sha1, listed} = JSON.parse(d);
-		let searchText = listed.map(function(d) {
-			return [d.title || '', d.description || '', d.tags || ''].join(' ');
-		}).join(' ')
-		let title = listed[0].title;
+		d = d.replace(/<a href="(.*?)">/g, "<a href='$1'>");
+		let {url,sha1, sources, props} = JSON.parse(d);
+		let searchText = 'title,description,tags'.split(',').reduce((acc, key) => {
+			let arr = props[key];
+			return acc + ' ' + (arr || []).join(' ');
+		}, '');
+		let title = props.title[0];
 		let thumbSrc = '../dist/assets/url_thumbs/tn_'+sha1+'.jpg';
 		return {title, url, thumbSrc,searchText};
 	});
