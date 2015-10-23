138037d882df745c9f507fa886b714981da9a3bc
Marielle Lange <mlange@widged.com>
2015-10-23 18:14:36 +1300

    [styling] adding license and cost tags

--- dist/bundle.es5.js
+++ dist/bundle.es5.js
@@ -19832,6 +19832,7 @@
 				}
 
 				// <a href={url}><svg width="16" height="16" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1408 928v320q0 119-84.5 203.5t-203.5 84.5h-832q-119 0-203.5-84.5t-84.5-203.5v-832q0-119 84.5-203.5t203.5-84.5h704q14 0 23 9t9 23v64q0 14-9 23t-23 9h-704q-66 0-113 47t-47 113v832q0 66 47 113t113 47h832q66 0 113-47t47-113v-320q0-14 9-23t23-9h64q14 0 23 9t9 23zm384-864v512q0 26-19 45t-45 19-45-19l-176-176-652 652q-10 10-23 10t-23-10l-114-114q-10-10-10-23t10-23l652-652-176-176q-19-19-19-45t19-45 45-19h512q26 0 45 19t19 45z"/></svg></a>
+				// based on http://codepen.io/doonnn/pen/QbBKxv?editors=110
 				return _react2['default'].createElement(
 					'wg-bookmark-card',
 					null,
@@ -19852,9 +19853,12 @@
 								{ className: 'card-caption' },
 								_react2['default'].createElement(
 									'div',
-									{ className: 'card-icons' },
-									licenseIcon ? _react2['default'].createElement('i', { id: 'like-btn', className: 'fa ' + licenseIcon }) : '',
-									hasCost ? _react2['default'].createElement('i', { id: 'like-btn', className: 'fa fa-usd' }) : ''
+									{ className: 'card-action' },
+									_react2['default'].createElement(
+										'a',
+										{ href: url, target: 'blank' },
+										_react2['default'].createElement('i', { id: 'action-btn', className: 'fa fa-angle-right' })
+									)
 								),
 								_react2['default'].createElement(
 									'div',
@@ -19872,18 +19876,30 @@
 								)
 							),
 							_react2['default'].createElement(
-								'a',
-								{ href: url, target: 'blank' },
-								_react2['default'].createElement(
+								'div',
+								{ className: 'card-tags' },
+								licenseIcon ? _react2['default'].createElement(
 									'div',
-									{ className: 'card-outmore' },
+									{ className: 'tag-wrapper' },
+									_react2['default'].createElement('i', { className: 'license fa ' + licenseIcon }),
+									' ',
 									_react2['default'].createElement(
-										'h5',
+										'em',
 										null,
-										'Explore'
-									),
-									_react2['default'].createElement('i', { id: 'outmore-icon', className: 'fa fa-angle-right' })
-								)
+										'license'
+									)
+								) : '',
+								hasCost ? _react2['default'].createElement(
+									'div',
+									{ className: 'tag-wrapper' },
+									_react2['default'].createElement('i', { className: 'cost fa fa-usd' }),
+									' ',
+									_react2['default'].createElement(
+										'em',
+										null,
+										'cost'
+									)
+								) : ''
 							)
 						)
 					)
--- src/components/wg-bookmarks-explorer/BookmarksExplorer.js
+++ src/components/wg-bookmarks-explorer/BookmarksExplorer.js
@@ -36,6 +36,7 @@ class BookmarkCard extends Component {
 		if(!licenseIcon) { console.log(license); }
 
 		// <a href={url}><svg width="16" height="16" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1408 928v320q0 119-84.5 203.5t-203.5 84.5h-832q-119 0-203.5-84.5t-84.5-203.5v-832q0-119 84.5-203.5t203.5-84.5h704q14 0 23 9t9 23v64q0 14-9 23t-23 9h-704q-66 0-113 47t-47 113v832q0 66 47 113t113 47h832q66 0 113-47t47-113v-320q0-14 9-23t23-9h64q14 0 23 9t9 23zm384-864v512q0 26-19 45t-45 19-45-19l-176-176-652 652q-10 10-23 10t-23-10l-114-114q-10-10-10-23t10-23l652-652-176-176q-19-19-19-45t19-45 45-19h512q26 0 45 19t19 45z"/></svg></a>
+		// based on http://codepen.io/doonnn/pen/QbBKxv?editors=110
 		return (
 			<wg-bookmark-card>
 				<LazyLoader height={200} threshold={100} onVisible={onLazyLoaded}>
@@ -45,21 +46,18 @@ class BookmarkCard extends Component {
 							 <div className="card-img-placeholder"></div>
 						 </div>
 						 <div className="card-caption">
-							 <div className='card-icons'>
-							 {licenseIcon    ? <i id="like-btn" className={'fa ' + licenseIcon}></i> : '' }
-							 {hasCost ? <i id="like-btn" className="fa fa-usd"></i> : '' }
+							  <div className='card-action'>
+								 <a href={url} target="blank"><i id="action-btn" className="fa fa-angle-right"></i></a>
 							 </div>
 							 <div className="card-caption-inner">
 								 <h1>{title}</h1>
 								 <span className="date">{agency}</span>
 							 </div>
 						 </div>
-						 <a href={url} target="blank">
-						 <div className="card-outmore">
-							 <h5>Explore</h5>
-							 <i id="outmore-icon" className="fa fa-angle-right"></i>
+						 <div className="card-tags">
+							 {licenseIcon    ? <div className="tag-wrapper"><i className={'license fa ' + licenseIcon}></i> <em>license</em></div> : '' }
+							 {hasCost    ? <div className="tag-wrapper"><i className="cost fa fa-usd"></i> <em>cost</em></div> : '' }
 						 </div>
-					 </a>
 					 </div>
 				</LazyLoader>
 			</wg-bookmark-card>
